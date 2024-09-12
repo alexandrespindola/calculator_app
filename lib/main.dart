edc8ce5ba663c8f3cbea8f5c52b57b14c3484306
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-09-12 11:44:47 +0200

    Add configuration files for macOS and Linux platforms

--- lib/main.dart
+++ lib/main.dart
@@ -24,206 +24,204 @@ class _MyAppState extends State<MyApp> {
           ),
           backgroundColor: Colors.blue[900],
         ),
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          color: Colors.blue[200],
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: ConstrainedBox(
-                constraints: const BoxConstraints(
-                  maxWidth: 400,
-                ),
-                child: Container(
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.blue[100],
+        body: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            color: Colors.blue[200],
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ConstrainedBox(
+                  constraints: const BoxConstraints(
+                    maxWidth: 400,
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      children: [
-                        const Spacer(
-                          flex: 1,
-                        ),
-                        const Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Text(
-                              '0',
-                              style: TextStyle(
-                                fontSize: 100,
-                                fontWeight: FontWeight.bold,
+                  child: Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.blue[100],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          const Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                '0',
+                                style: TextStyle(
+                                  fontSize: 100,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            CalcButton(
-                              text: 'AC',
-                              onPressed: () {
-                                print('AC');
-                              },
-                              color: Colors.white,
-                            ),
-                            CalcButton(
-                              text: 'Back',
-                              onPressed: () {
-                                print('Back');
-                              },
-                              color: Colors.orange,
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            CalcButton(
-                              text: '7',
-                              onPressed: () {
-                                print('7');
-                              },
-                              color: Colors.white,
-                            ),
-                            CalcButton(
-                              text: '8',
-                              onPressed: () {
-                                print('8');
-                              },
-                              color: Colors.white,
-                            ),
-                            CalcButton(
-                              text: '9',
-                              onPressed: () {
-                                print('9');
-                              },
-                              color: Colors.white,
-                            ),
-                            CalcButton(
-                              text: '/',
-                              onPressed: () {
-                                print('/');
-                              },
-                              color: Colors.orange,
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            CalcButton(
-                              text: '4',
-                              onPressed: () {
-                                print('4');
-                              },
-                              color: Colors.white,
-                            ),
-                            CalcButton(
-                              text: '5',
-                              onPressed: () {
-                                print('5');
-                              },
-                              color: Colors.white,
-                            ),
-                            CalcButton(
-                              text: '6',
-                              onPressed: () {
-                                print('6');
-                              },
-                              color: Colors.white,
-                            ),
-                            CalcButton(
-                              text: 'X',
-                              onPressed: () {
-                                print('X');
-                              },
-                              color: Colors.orange,
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            CalcButton(
-                              text: '1',
-                              onPressed: () {
-                                print('1');
-                              },
-                              color: Colors.white,
-                            ),
-                            CalcButton(
-                              text: '2',
-                              onPressed: () {
-                                print('2');
-                              },
-                              color: Colors.white,
-                            ),
-                            CalcButton(
-                              text: '3',
-                              onPressed: () {
-                                print('3');
-                              },
-                              color: Colors.white,
-                            ),
-                            CalcButton(
-                              text: '-',
-                              onPressed: () {
-                                print('-');
-                              },
-                              color: Colors.orange,
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            CalcButton(
-                              text: '0',
-                              onPressed: () {
-                                print('0');
-                              },
-                              color: Colors.white,
-                            ),
-                            CalcButton(
-                              text: ',',
-                              onPressed: () {
-                                print(',');
-                              },
-                              color: Colors.white,
-                            ),
-                            CalcButton(
-                              text: '=',
-                              onPressed: () {
-                                print('=');
-                              },
-                              color: Colors.orange,
-                            ),
-                            CalcButton(
-                              text: '+',
-                              onPressed: () {
-                                print('+');
-                              },
-                              color: Colors.orange,
-                            ),
-                          ],
-                        ),
-                      ],
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              CalcButton(
+                                text: 'AC',
+                                onPressed: () {
+                                  print('AC');
+                                },
+                                color: Colors.white,
+                              ),
+                              CalcButton(
+                                text: 'Back',
+                                onPressed: () {
+                                  print('Back');
+                                },
+                                color: Colors.orange,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              CalcButton(
+                                text: '7',
+                                onPressed: () {
+                                  print('7');
+                                },
+                                color: Colors.white,
+                              ),
+                              CalcButton(
+                                text: '8',
+                                onPressed: () {
+                                  print('8');
+                                },
+                                color: Colors.white,
+                              ),
+                              CalcButton(
+                                text: '9',
+                                onPressed: () {
+                                  print('9');
+                                },
+                                color: Colors.white,
+                              ),
+                              CalcButton(
+                                text: '/',
+                                onPressed: () {
+                                  print('/');
+                                },
+                                color: Colors.orange,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              CalcButton(
+                                text: '4',
+                                onPressed: () {
+                                  print('4');
+                                },
+                                color: Colors.white,
+                              ),
+                              CalcButton(
+                                text: '5',
+                                onPressed: () {
+                                  print('5');
+                                },
+                                color: Colors.white,
+                              ),
+                              CalcButton(
+                                text: '6',
+                                onPressed: () {
+                                  print('6');
+                                },
+                                color: Colors.white,
+                              ),
+                              CalcButton(
+                                text: 'X',
+                                onPressed: () {
+                                  print('X');
+                                },
+                                color: Colors.orange,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              CalcButton(
+                                text: '1',
+                                onPressed: () {
+                                  print('1');
+                                },
+                                color: Colors.white,
+                              ),
+                              CalcButton(
+                                text: '2',
+                                onPressed: () {
+                                  print('2');
+                                },
+                                color: Colors.white,
+                              ),
+                              CalcButton(
+                                text: '3',
+                                onPressed: () {
+                                  print('3');
+                                },
+                                color: Colors.white,
+                              ),
+                              CalcButton(
+                                text: '-',
+                                onPressed: () {
+                                  print('-');
+                                },
+                                color: Colors.orange,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              CalcButton(
+                                text: '0',
+                                onPressed: () {
+                                  print('0');
+                                },
+                                color: Colors.white,
+                              ),
+                              CalcButton(
+                                text: ',',
+                                onPressed: () {
+                                  print(',');
+                                },
+                                color: Colors.white,
+                              ),
+                              CalcButton(
+                                text: '=',
+                                onPressed: () {
+                                  print('=');
+                                },
+                                color: Colors.orange,
+                              ),
+                              CalcButton(
+                                text: '+',
+                                onPressed: () {
+                                  print('+');
+                                },
+                                color: Colors.orange,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
